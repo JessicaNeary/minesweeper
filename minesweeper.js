5121dbb51385c145fde59f5aac3dffb7e5842362
Jess Neary <Caneary@hotmail.com>
2016-11-07 14:15:45 +1300

    blocked

--- minesweeper.js
+++ minesweeper.js
@@ -2,18 +2,26 @@ document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
 var board = {
-  cells: [new cell(1,1), new cell(0,1), new cell(1,0), new cell(0,0), new cell(0,2), new cell(1,2), new cell(2,0), new cell(2,1), new cell(2,2)]
+  cells: [
+		new cell(1,1),
+		new cell(0,1),
+		new cell(1,0),
+		new cell(0,0),
+		new cell(0,2),
+		new cell(1,2),
+		new cell(2,0),
+		new cell(2,1),
+		new cell(2,2)]
 }
 
 function cell(r,c){
   this.row = r;
   this.col = c;
-  this.isMine = false;
+  this.isMine = true;
   this.hidden = true;
-  this.surroundingMines = 0;
 }
 
-// function mine(r.c){
+// function mine(r,c){
 //   this.row = r;
 //   this.col = c;
 //   this.isMine = true;
@@ -22,10 +30,10 @@ function cell(r,c){
 
 function startGame () {
   // Don't remove this function call: it makes the game work!
-  // for(i=0; i<board.cells.length; i++){
-  //   var mines = countSurroundingMines(board.cells[i])
-  //   board.cells[i].surroundingMines = mines;
-  // }
+  for(i=0; i<board.cells.length; i++){
+    var mines = countSurroundingMines(board.cells[i])
+    board.cells[i].surroundingMines = mines;
+  }
   lib.initBoard()
 
 }
@@ -58,12 +66,14 @@ function checkForWin () {
 // It will return cell objects in an array. You should loop through
 // them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines (cell) {
-  // var count;
-  // var surroundingCells = lib.getSurroundingCells(cell.row, cell.col);
-  // for(var i=0; i<=surroundingCells.length; i++){
-  //   if(board.cells[surroundingCells[i]].isMine === true){
-  //     count++;
-  //   }
-  // }
-  // return count;
+  var count;
+  var surroundingCells = lib.getSurroundingCells(cell.row, cell.col);
+  for(var i=0; i<=surroundingCells.length; i++){
+		console.log(surroundingCells[i]);
+		console.log(surroundingCells[i].isMine);
+    if(surroundingCells[i].isMine){
+      count++;
+    }
+  }
+  return count;
 }
